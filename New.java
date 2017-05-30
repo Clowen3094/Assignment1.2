73d2a7b96670b688a35df5acef5d8deaabc15962
Clowen3094 <kaninia207@gmail.com>
2017-05-30 11:43:00 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,18 @@
+class New {
+static int a = 20;
+static int b = 10;
+public static void main(String args[]){
+int b= a-- - --a;
+System.out.println(b);
+
+int c =a--;
+System.out.println(c);
+
+int d =a >> 2;
+System.out.println(d);
+
+int e = a&b;
+System.out.println(e);
+
+}
+}
